51986b0e31fa174dab4f6a26257b91480abc7a7f
Leonardo dos Santos Quintania <leonardo.quintania@totvs.com.br>
2021-03-03 23:50:16 -0300

    Adicionado arquivos conforme aula.

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'postgres',
+	password: 'Postgres2018!',
+	database: 'gobarber',
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
