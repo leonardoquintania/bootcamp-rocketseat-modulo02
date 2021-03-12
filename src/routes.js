26e6a3a3d5faa8a1d9dcd125b1d3f9922e632d68
Leonardo dos Santos Quintania <leonardo.quintania@totvs.com.br>
2021-03-12 09:20:07 -0300

    Adicionado conforme aulas sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+	res.json({ message: 'Hello World' });
+});
+
+routes.post('/users', UserController.store);
 
 export default routes;
